cea239330d38f9daca3347c1993fdd768d1d3843
Ian Fu <peaceian9527@gmail.com>
2023-12-05 16:54:24 +0800

    fourthcommit

--- weather/views.py
+++ weather/views.py
@@ -4,22 +4,43 @@ from bs4 import BeautifulSoup
 import json
 from json import loads
 import math
-import urllib.request
+import urllib.request #讀取url
 import re
-import urllib.parse
+import urllib.parse #解析url
 from django.http import HttpResponse
+import random
 import urllib3
 urllib3.disable_warnings()#Maybe appear InsecureRequestWarning, this could it disable.
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Create your views here.
+
+user_agents = [
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+ "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+ "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+ "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+ "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+ "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+ "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+ "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+ "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+ "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+ "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+ "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+ "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        ]
+headers = {'user-agent': random.choice(user_agents)} #偽裝使用者
 user_key = 'CWB-869C7631-CE97-4263-9AF8-635DF71E8A67'
 
 def wr8(request):
     doc_name = 'F-C0032-001'
     url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/%s?Authorization=%s" % (doc_name,user_key)
-    headers = {'Connection':'close'}
+    #headers = {'Connection':'close'}
     #requests setting remove SSL certificate.
     data = requests.get(url,headers=headers,verify=False) #requests json檔內容為文字
     print(data)
@@ -81,7 +102,7 @@ def wr8(request):
 def realtimeweather(request):
     doc_name = 'O-A0001-001'
     url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/%s?Authorization=%s" % (doc_name,user_key)
-    headers = {'Connect':'close'}
+    #headers = {'Connect':'close'}
     data = requests.get(url,headers=headers,verify=False)
     data_json = data.json()
     locations = data_json['records']['Station']
