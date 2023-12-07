5391288d982a07d65bfcf72ce72b8fd453d3e655
Ian Fu <peaceian9527@gmail.com>
2023-12-07 12:42:18 +0800

    ninethcommit

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]#whitenoice new added
+]#whitenoise new added
 
 ROOT_URLCONF = 'weatherapp.urls'
 
